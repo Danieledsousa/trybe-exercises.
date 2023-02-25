d80898501ab78167d6947a5dfcfe2086a125a00b
Daniele Silva de Sousa <ds65411@gmail.com>
2023-02-24 21:03:30 -0300

    operadores logicos exercicio 2

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-operadores-logicos/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-operadores-logicos/script.js
@@ -2,15 +2,26 @@ let currentHour = 18;
 let message = "";
 
 if(currentHour >= 22){
-    message = "Não deveríamos comer nada, é hora de dormir"
+    message = "Não deveríamos comer nada, é hora de dormir";
 }
 else if(currentHour >= 18 && currentHour < 22){
-    message = "Rango da noite, vamos jantar :D"
+    message = "Rango da noite, vamos jantar :D";
 }
 else if(currentHour >= 14 && currentHour < 18){
-    message = "Vamos fazer um bolo pro café da tarde?"
+    message = "Vamos fazer um bolo pro café da tarde?";
 }
 else if(currentHour >= 11 && currentHour < 14){
-    message = "Hora do almoço!!!"
+    message = "Hora do almoço!!!";
 }
-console.log(message)
+console.log(message);
+
+
+
+let weekDay = "quarta-feira";
+
+if(weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira"){
+    console.log( "Oba, mais um dia de aprendizado na Trybe :D");
+}
+else {
+    console.log("FINALMENTE, descanso merecido! UwU!")
+}
