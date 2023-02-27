4ce371d537f4cc0cbdede06eb54f6e2cbfae2fde
Daniele Silva de Sousa <ds65411@gmail.com>
2023-02-27 16:07:37 -0300

    retornando salario liquido

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-praticas-javascript/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-praticas-javascript/script.js
@@ -141,6 +141,36 @@ if (costValue >= 0 && saleValue >= 0) {
 };
 
 
+let aliquotINSS;
+let aliquotIR;
+let grossSalary = 3000;
+
+if(grossSalary <= 1556.94){
+    aliquotINSS = grossSalary * 0.08
+}else if(grossSalary >= 1556.95 && grossSalary <= 2594.92){
+    aliquotINSS = grossSalary * 0.09
+}else if(grossSalary >= 2594.93 && grossSalary <= 5189.82){
+    aliquotINSS = grossSalary * 0.11
+}else if(grossSalary > 5189.82){
+    aliquotINSS = 570.88
+}
+
+
+let baseSalary = grossSalary - aliquotINSS
+
+if(baseSalary <= 1903.98){
+    aliquotIR = 0;
+}else if(baseSalary <= 2826.65){
+    aliquotIR = (baseSalary * 0.075) - 142.80
+}else if(baseSalary <= 351.05){
+    aliquotIR = (baseSalary * 0.15) - 354.80
+}else if(baseSalary <= 4664.68){
+    aliquotIR = (baseSalary * 0.225) - 636.13
+}else if(baseSalary > 4664.68){
+    aliquotIR = (baseSalary * 0.275) - 869.36
+}
+
+console.log(`Salario: ${baseSalary - aliquotIR}`)
 
 
 
