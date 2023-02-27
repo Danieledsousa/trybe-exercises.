2c8e529089d188263fddd55e5f719d0d59fddf2a
Daniele Silva de Sousa <ds65411@gmail.com>
2023-02-27 11:42:55 -0300

    o maior entre dois numeros

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-praticas-javascript/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-praticas-javascript/script.js
@@ -12,3 +12,15 @@ console.log(subtracao);
 console.log(multiplicacao);
 console.log(divisao);
 console.log(modulo);
+
+
+let number1 = 400;
+let number2 = 3000;
+
+if(number1 > number2){
+    console.log(`O maior numero é ${number1} e o menor é ${number2}`)
+}
+else if (number2 > number1){
+    console.log(`O maior numero é ${number2} e o menor é ${number1}`)
+}
+
