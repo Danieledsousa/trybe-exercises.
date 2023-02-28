f6d2412746dcf08b85f476c5ddcb545476d93b7f
Daniele Silva de Sousa <ds65411@gmail.com>
2023-02-28 19:36:16 -0300

    imprimindo numeros de 1 a 25

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/pratica-array-e-for/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/pratica-array-e-for/script.js
@@ -56,4 +56,12 @@ for (let index = 0; index < numbers.length; index+=1) {
     }
     
 }
-console.log(menor)
+console.log(menor);
+
+
+let array = []
+for (let index = 1; index <= 25; index+=1) {
+    array.push(index)
+}
+console.log(array)
+
