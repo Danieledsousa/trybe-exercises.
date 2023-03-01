9f8b29e8134b17d08f9e16845f7885ddde6bdb91
Daniele Silva de Sousa <ds65411@gmail.com>
2023-03-01 12:12:55 -0300

    subtraindo valor do saldo

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/pratica-de-funcoes/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/pratica-de-funcoes/script.js
@@ -1,7 +1,12 @@
 let balance = 300;
 
 function addValue(value) {
-    return balance + value
+    return balance + value;
 }
 console.log(addValue(80));
 
+
+function subtractValue(value) {
+    return balance - value;
+}
+console.log(subtractValue(50));
