d2b74d4ebabf2a193ce9d14f4b58c1efdb48efcc
Daniele Silva de Sousa <ds65411@gmail.com>
2023-03-04 10:33:23 -0300

    imprimindo numeros multiplicados pelo anterior

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-bonus-ordenacao-bubble-sort/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-bonus-ordenacao-bubble-sort/script.js
@@ -5,3 +5,14 @@ console.log(numbers);
 
 numbers.sort((a, b) => b - a);
 console.log(numbers)
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multipliedNumbers = [];
+for (let index = 0; index < numbers2.length; index+=1) {
+    if (index < numbers2.length - 1) {
+        multipliedNumbers.push(numbers2[index] * numbers2[index+1]);
+    } else {
+        multipliedNumbers.push(numbers2[index] * numbers2[index]);
+    }
+}
+console.log(multipliedNumbers)
