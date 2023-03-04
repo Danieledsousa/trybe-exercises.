a4510f6bff550abfd74d4828610e9b11ee2067f1
Daniele Silva de Sousa <ds65411@gmail.com>
2023-03-04 16:31:19 -0300

    acessando valores do array livrosFavoritos

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicio-objetos-e-for-in/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicio-objetos-e-for-in/script.js
@@ -30,4 +30,19 @@ for (let key in info) {
     } else {
         console.log(info[key] + ' e ' + info2[key]);
     }
-}
+};
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+    {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+    },
+    ],
+};
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
