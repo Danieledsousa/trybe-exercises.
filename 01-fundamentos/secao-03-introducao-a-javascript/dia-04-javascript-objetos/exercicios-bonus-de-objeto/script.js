93263e06cb9b89ae680c77585b9f153ac342bf25
Daniele Silva de Sousa <ds65411@gmail.com>
2023-03-05 09:38:39 -0300

    agrupando objetos em outro objeto

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios-bonus-de-objeto/script.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios-bonus-de-objeto/script.js
@@ -38,3 +38,10 @@ function objectValue(obj) {
     return Object.values(obj);
 }
 console.log(objectValue(lesson3))
+
+const allLessons = Object.assign({}, { 
+    lesson1: lesson1, 
+    lesson2: lesson2, 
+    lesson3: lesson3, 
+});
+console.log(allLessons);
