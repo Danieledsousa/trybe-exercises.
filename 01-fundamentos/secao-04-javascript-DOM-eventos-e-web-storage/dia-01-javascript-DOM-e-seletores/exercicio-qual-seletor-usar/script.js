18deb3c17107ba780cf754e5397f491218e53915
Daniele Silva de Sousa <ds65411@gmail.com>
2023-03-08 19:11:44 -0300

    dom/seletores

---
+++ 01-fundamentos/secao-04-javascript-DOM-eventos-e-web-storage/dia-01-javascript-DOM-e-seletores/exercicio-qual-seletor-usar/script.js
@@ -0,0 +1,24 @@
+const containerHeader = document.getElementById('header-container');
+containerHeader.style.backgroundColor = 'green';
+
+const sectionEmergencyTasks = document.querySelector('.emergency-tasks');
+sectionEmergencyTasks.style.backgroundColor = 'pink';
+
+const sectionNoEmergencyTasks = document.querySelector('.no-emergency-tasks');
+sectionNoEmergencyTasks.style.backgroundColor = 'yellow';
+
+const title = document.querySelector('.list-emergency .list1');
+title.style.backgroundColor = 'purple';
+
+const title2 = document.querySelector('.list-emergency .list2');
+title2.style.backgroundColor = 'purple';
+
+const title3 = document.querySelector('.list-no-emergency .list3');
+title3.style.backgroundColor = 'black';
+
+const title4 = document.querySelector('.list-no-emergency .list4');
+title4.style.backgroundColor = 'black';
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'green'
+
