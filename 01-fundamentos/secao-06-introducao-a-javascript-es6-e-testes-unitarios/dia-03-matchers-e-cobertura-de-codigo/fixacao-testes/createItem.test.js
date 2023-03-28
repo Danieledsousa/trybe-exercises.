8a1c9a3ce05cffb70142135cfefe709106ba5245
Daniele Silva de Sousa <ds65411@gmail.com>
2023-03-28 12:24:43 -0300

    identação do codigo

--- 01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ 01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -1,31 +1,42 @@
 const createItem = require("./createItem");
 
-describe('função que cria itens para a lista', () => {
-    it('cria um item válido', () => {
-        const returnItem = { name: 'banana', unit: 'kg', price: 1.99, quantity: 20 };
-        const itemCreated = createItem('banana', 'kg', 1.99, 20);
-        expect(itemCreated).toEqual(returnItem);
-    });
+describe("função que cria itens para a lista", () => {
+  it("cria um item válido", () => {
+    const returnItem = {
+      name: "banana",
+      unit: "kg",
+      price: 1.99,
+      quantity: 20,
+    };
+    const itemCreated = createItem("banana", "kg", 1.99, 20);
+    expect(itemCreated).toEqual(returnItem);
+  });
 
-    it('Teste se a função utiliza zero como quantidade padrão.', () => {
-        const returnItem = 0;
-        const itemCreated = createItem('banana', 'kg', 1.99);
-        expect(itemCreated).toHaveProperty('quantity', returnItem);
-    });
+  it("Teste se a função utiliza zero como quantidade padrão.", () => {
+    const returnItem = 0;
+    const itemCreated = createItem("banana", "kg", 1.99);
+    expect(itemCreated).toHaveProperty("quantity", returnItem);
+  });
 
-    it('Teste se a função lança algum erro quando não recebe parâmetros.', () => {
-        expect(() =>  createItem()).toThrow();
-    });
+  it("Teste se a função lança algum erro quando não recebe parâmetros.", () => {
+    expect(() => createItem()).toThrow();
+  });
 
-    it('Teste se a função lança um erro se o nome do item não é uma string.', () => {
-        expect(() =>  createItem(1.99, 'kg', 'banana' , 20)).toThrow(new Error('O nome do item deve ser uma string'));
-    });
+  it("Teste se a função lança um erro se o nome do item não é uma string.", () => {
+    expect(() => createItem(1.99, "kg", "banana", 20)).toThrow(
+      new Error("O nome do item deve ser uma string")
+    );
+  });
 
-    it('Teste se a função lança um erro se o preço do item é negativo ou zero.', () => {
-        expect(() =>  createItem('banana', 'kg', -0.01)).toThrow(new Error('O preço do item deve ser maior que zero'));
-    });
+  it("Teste se a função lança um erro se o preço do item é negativo ou zero.", () => {
+    expect(() => createItem("banana", "kg", -0.01)).toThrow(
+      new Error("O preço do item deve ser maior que zero")
+    );
+  });
 
-    it('Teste se a função lança um erro se o preço do item é negativo ou zero.', () => {
-        expect(() =>  createItem('banana', 'kg', 0.00)).toThrow(new Error('O preço do item deve ser maior que zero'));
-    });
-})
+  it("Teste se a função lança um erro se o preço do item é negativo ou zero.", () => {
+    expect(() => createItem("banana", "kg", 0.0)).toThrow(
+      new Error("O preço do item deve ser maior que zero")
+    );
+  });
+});
