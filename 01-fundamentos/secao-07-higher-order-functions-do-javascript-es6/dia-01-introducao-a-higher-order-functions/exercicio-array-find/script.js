338a8ea0fcd761c9875021bdbeec194344c6d71f
Daniele Silva de Sousa <ds65411@gmail.com>
2023-04-03 13:12:10 -0300

    tamanho de letra igual a 5

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-array-find/script.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicio-array-find/script.js
@@ -1,5 +1,10 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const divisibleNumbers = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
-
 console.log(divisibleNumbers);
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const fiveLetter = names.find((name) => name.length === 5);
+console.log(fiveLetter);
