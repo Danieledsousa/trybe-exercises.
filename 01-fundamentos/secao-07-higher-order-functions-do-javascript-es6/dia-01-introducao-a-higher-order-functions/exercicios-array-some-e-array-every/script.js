5b2289d5fdd90a455b11f6db60922c9def14312a
Daniele Silva de Sousa <ds65411@gmail.com>
2023-04-03 14:11:44 -0300

    idade maior ou igual à mínima

--- 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios-array-some-e-array-every/script.js
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/exercicios-array-some-e-array-every/script.js
@@ -6,3 +6,19 @@ const hasName = (arr, name) => {
 
 console.log(hasName(names, 'Ana'));
 console.log(hasName(names, 'Pedro'));
+
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, number) => {
+    return arr.every((allName) => allName.age >= number)
+  }
+  
+  console.log(verifyAges(people, 18));
+  console.log(verifyAges(people, 14));
