94c7ccc3eb484209bfd3074bae751366cbcb9329
Daniele Silva de Sousa <ds65411@gmail.com>
2023-04-10 11:19:00 -0300

    exercicio desestruturação de array

---
+++ 01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercicio-array-destructuring/script.js
@@ -0,0 +1,36 @@
+// exercise 01
+const primeNumbers = [17, 23, 37] 
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2])  // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [firstNumber, secondNumber, thirdNumber] = primeNumbers;
+
+sum(firstNumber, thirdNumber);
+
+
+//exercise 02
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida, animal, bebida] = [bebida, comida, animal]
+console.log(comida, animal, bebida)
+
+
+
+//exercise 03
+ let numerosPares = [1, 3, 5, 6, 8, 10, 12]; 
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+const [,,, ...numerosPares] = numerosPares;
