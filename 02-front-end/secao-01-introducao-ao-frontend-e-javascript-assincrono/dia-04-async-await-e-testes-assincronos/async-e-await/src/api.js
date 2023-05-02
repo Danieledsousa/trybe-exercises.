797e4914399f61816e466dbd2ad62010991aad72
Daniele Silva de Sousa <ds65411@gmail.com>
2023-05-02 15:28:30 -0300

    finalizando secao 01

--- 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-04-async-await-e-testes-assincronos/async-e-await/src/api.js
+++ 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-04-async-await-e-testes-assincronos/async-e-await/src/api.js
@@ -1,13 +1,23 @@
+export async function fetchApi(cep) {
+    try {
+    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`) 
+    const data = await result.json()
+    return data
+    } catch(error) {
+        return error.message
+    }
+
+} 
+/* 
 export const fetchApi = (cep) => {
     const result = fetch(`https://viacep.com.br/ws/${cep}/json/`) 
     .then((resp)=> resp.json())
     .then((dados) => {
-        console.log(dados)
         return dados
+        
     });
     return result 
-}
-
+}  */
 
 
 
--- 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-04-async-await-e-testes-assincronos/async-e-await/src/main.js
+++ 02-front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-04-async-await-e-testes-assincronos/async-e-await/src/main.js
@@ -1,16 +1,21 @@
 import { fetchApi } from "./api.js";
 import { renderCep } from "./dom.js";
 
+
 const inputForm = document.querySelector('input');
 const buttonForm = document.querySelector('button');
 
 async function handleSearch(event){
     event.preventDefault();
-    const result = await fetchApi(inputForm.value);
-    result.toString()
-    renderCep(result)
+   try{
+       const result = await fetchApi(inputForm.value);
+       result.toString()
+       renderCep(result)
+       
+   }catch (error) {
+    return error.message
+   
+
 }
-
+}
 buttonForm.addEventListener('click', handleSearch);
-
-
