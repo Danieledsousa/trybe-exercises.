616c20b2a05ca2725ea1b2386f5b695d647fc140
Daniele Silva de Sousa <ds65411@gmail.com>
2023-05-08 10:31:53 -0300

    funcoes com acesso ao objeto this

--- 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/exercicio-com-eventos-react/fancy-buttons/src/App.js
+++ 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/exercicio-com-eventos-react/fancy-buttons/src/App.js
@@ -1,30 +1,29 @@
 import './App.css';
 import React from 'react';
 
-function handleClick() {
-  console.log('Botão clicado')
-}
-
-function handleButtonOne() {
-  console.log('Botão clicado')
-}
-
-function handleButtonTwo() {
-  console.log('Botão clicado')
-}
-
-function handleButtonThree() {
-  console.log('Botão clicado')
-}
-
 class App extends React.Component {
+  handleClick = () => {
+    console.log('Botão clicado')
+  }
+  
+  handleButtonOne = () => {
+    console.log('Botão clicado')
+  }
+  
+  handleButtonTwo = () => {
+    console.log('Botão clicado')
+  }
+  
+  handleButtonThree = () => {
+    console.log('Botão clicado')
+  }
   render() {
   return (
     <div className="App">
-      <button onclick={ handleClick }>Botao</button>
-      <button onclick={ handleButtonOne }>Botao</button>
-      <button onclick={ handleButtonTwo }>Botao</button>
-      <button onclick={ handleButtonThree }>Botao</button>
+      <button onclick={ this.handleClick }>Botao</button>
+      <button onclick={ this.handleButtonOne }>Botao 1</button>
+      <button onclick={ this.handleButtonTwo }>Botao 2</button>
+      <button onclick={ this.handleButtonThree }>Botao 3</button>
     </div>
   );
   }
